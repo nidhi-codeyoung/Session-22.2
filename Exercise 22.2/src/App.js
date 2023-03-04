ad9546140c74e5b61d94f0a0783496cb34d9b408
Nidhi Satija <126676344+nidhi-codeyoung@users.noreply.github.com>
2023-03-04 19:00:22 +0530

    Add files via upload

---
+++ Exercise 22.2/src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+
+function App(){
+return (
+<>
+
+<h2></h2>
+
+<header>
+  <h3>Technical Blogging Website</h3>
+</header>
+
+<div>
+  <nav>
+    <ul>
+      <li><a href="#">Tech Blogs</a></li>
+      <li><a href="#">Tech News</a></li>
+      <li><a href="#">Tech Trends</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>The Tech Era: For Students & Others</h1>
+    <p>Technological knowlegde enables students to explore new subjects and deepen their understanding of difficult concepts, particularly in STEM. Through the use of technology inside and outside the classroom, students can gain 21st-century technical skills necessary for future occupations.</p>
+    
+    <p>Technology is transforming the ages of life and it will impact life of every 
+    indivual irrespective of their age, profession and interest. 
+    So, I'm Jack introducing you all with this user-freindly website for enriching your 
+    knowledge about Technology. 
+  </p> 
+   <h4>
+   Tuned for such more updates. </h4>
+     
+  </article>
+</div>
+
+<footer>
+  <p>Technical Website Footer </p>
+  <p>Copyright 2022:2023</p>
+  <p>Made by Jack</p>
+</footer>
+
+</>
+
+)
+}
+export default App;
